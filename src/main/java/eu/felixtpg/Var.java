396fa7269fa2da8a9f48d0e0d0e1f0871ecd55d8
felixtpg <bufex09@icloud.com>
2024-10-17 17:46:15 +0200

    update to version 1.2-BETA

--- src/main/java/eu/felixtpg/Var.java
+++ src/main/java/eu/felixtpg/Var.java
@@ -2,7 +2,7 @@ package eu.felixtpg;
 
 public class Var {
 
-    public static String VERSION = "v1.1-BETA";
+    public static String VERSION = "v1.2-BETA";
     public static final String SUPPORT_SERVER_LINK = "null";
 
 }
